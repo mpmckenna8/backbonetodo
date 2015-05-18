8f6a0bd584a3c820af93e77d61bfaa150164d571
mpmckenna8 <mpmckenna8@me.com>
2015-05-18 14:52:48 -0700

    Got a little todo app up and running.

---
+++ js/app.js
@@ -0,0 +1,10 @@
+// this if where the main directions will get called i thinks
+var app = app || {};
+var ENTER_KEY = 13;
+
+$(function() {
+
+  // Kick things off by creating the **App**.
+  new app.AppView();
+
+});
